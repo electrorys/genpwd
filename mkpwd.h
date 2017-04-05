32ca3ab99300b72b708ce4537d020b6e9e40dd86
Рысь <electrorys@ya.ru>
2017-04-05 10:29:54 +0700

    raise MKPWD_ROUNDS_MAX.

--- mkpwd.h
+++ mkpwd.h
@@ -1,6 +1,6 @@
 #define MKPWD_INPUT_MAX 1024
 #define MKPWD_OUTPUT_MAX 180
-#define MKPWD_ROUNDS_MAX 100000
+#define MKPWD_ROUNDS_MAX 10000000
 
 extern int mkpwd_passes_number, mkpwd_string_offset, mkpwd_password_length, mkpwd_output_format;
 char *mkpwd(const void *salt, size_t slen, const char **data);
