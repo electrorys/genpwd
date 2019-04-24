140873cd70f45ae0d878bd33636f5de53ebacdc4
Andrey Rys <rys@lynxlynx.ru>
2019-04-25 03:10:44 +0700

    Add defaults hash string to genpwd_defs.h.

--- genpwd_defs.h
+++ genpwd_defs.h
@@ -3,6 +3,8 @@
 
 gpwd_yesno genpwd_save_ids = YES;
 
+/* UXGyzZmMQJvDBIn61CAQLK82ffXihB4mUK/40oKQj9Y= */
+
 size_t genpwd_szsalt = 8;
 gpwd_byte genpwd_salt[GENPWD_MAX_SALT] = {
 	0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
