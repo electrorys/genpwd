electrorys/genpwd

commit cacb42dc3fe5c2c557af74a4babbd2ecc9751ec8

mkpwd: UNIV shall rely only on provided charset.

VERSION
@@ -1,1 +1,1 @@
-179
+180

mkpwd.c
@@ -126,7 +126,7 @@ int mkpwd(struct mkpwd_args *mkpwa)
 
 		s = bpw;
 		for (x = 0; x < mkpwa->length/2; x++) {
-_tryagainc1:		c = (char)tf_prng_range_r(rndata, 0x20, 0x7f);
+_tryagainc1:		c = (char)tf_prng_range_r(rndata, (TF_UNIT_TYPE)0x20, (TF_UNIT_TYPE)0x7f);
 			if (strchr(MKPWD_ALPHA_STRING, c)) {
 				*s = c;
 				s++;
@@ -134,7 +134,7 @@ _tryagainc1:		c = (char)tf_prng_range_r(rndata, 0x20, 0x7f);
 			else goto _tryagainc1;
 		}
 		for (; x < mkpwa->length; x++) {
-_tryagainc2:		c = (char)tf_prng_range_r(rndata, 0x20, 0x7f);
+_tryagainc2:		c = (char)tf_prng_range_r(rndata, (TF_UNIT_TYPE)0x20, (TF_UNIT_TYPE)0x7f);
 			if (strchr(MKPWD_DIGIT_STRING, c)) {
 				*s = c;
 				s++;
@@ -144,7 +144,7 @@ _tryagainc2:		c = (char)tf_prng_range_r(rndata, 0x20, 0x7f);
 
 		s = ret; d = bpw;
 		for (x = 0; x < mkpwa->length; x++) {
-_tryagainc3:		i = (size_t)tf_prng_range_r(rndata, 0, (TF_UNIT_TYPE)mkpwa->length-1);
+_tryagainc3:		i = (size_t)tf_prng_range_r(rndata, (TF_UNIT_TYPE)0, (TF_UNIT_TYPE)mkpwa->length-1);
 			if (d[i] == '\0') goto _tryagainc3;
 			*s = d[i];
 			s++;
@@ -159,17 +159,24 @@ _tryagainc3:		i = (size_t)tf_prng_range_r(rndata, 0, (TF_UNIT_TYPE)mkpwa->length
 		void *rndata;
 		char c, *s = ret;
 		size_t x;
-
-		if (mkpwa->charstart == '\0') mkpwa->charstart = 0x20;
-		if (mkpwa->charend == '\0') mkpwa->charend = 0x7f;
+		unsigned char S, E;
 
 		bpw = genpwd_realloc(bpw, TF_KEY_SIZE);
 		rndata = genpwd_malloc(tf_prng_datasize());
 
 		tf_prng_seedkey_r(rndata, bpw);
 
+		if (!mkpwa->charset) {
+			S = 0x20;
+			E = 0x7f;
+		}
+		else {
+			S = 1;
+			E = (unsigned char)UCHAR_MAX;
+		}
+
 		for (x = 0; x < mkpwa->length; x++) {
-_tryagainu:		c = (char)tf_prng_range_r(rndata, (TF_UNIT_TYPE)mkpwa->charstart, (TF_UNIT_TYPE)mkpwa->charend);
+_tryagainu:		c = (char)tf_prng_range_r(rndata, (TF_UNIT_TYPE)S, (TF_UNIT_TYPE)E);
 			if (mkpwa->charset) {
 				if (strchr(mkpwa->charset, c)) {
 					*s = c;

mkpwd.h
@@ -28,8 +28,6 @@ struct mkpwd_args {
 
 	short format;
 	char *charset;
-	char charstart;
-	char charend;
 	size_t passes;
 	size_t offset;
 	size_t length;

commit 1bc40a20110752f29d8a81efbf4991edfd9c946d

UNIV: fix important regression after cacb42d: return to charstart and charend (cs/ce) for char range selection.

VERSION
@@ -1,1 +1,1 @@
-184
+185

mkpwd.c
@@ -106,24 +106,17 @@ _tryagainc3:		i = (size_t)tf_prng_range_r(rndata, (TF_UNIT_TYPE)0, (TF_UNIT_TYPE
 	else if (mkpwa->format == MKPWD_FMT_UNIV) {
 		char c, *s;
 		size_t x;
-		unsigned char S, E;
 
 		bpw = genpwd_realloc(bpw, TF_KEY_SIZE);
 		rndata = genpwd_malloc(tf_prng_datasize());
 
 		tf_prng_seedkey_r(rndata, bpw);
 
-		if (!mkpwa->charset) {
-			S = 0x20;
-			E = 0x7f;
-		}
-		else {
-			S = 1;
-			E = (unsigned char)UCHAR_MAX;
-		}
+		if (mkpwa->cs == '\0') mkpwa->cs = 0x20;
+		if (mkpwa->ce == '\0') mkpwa->ce = 0x7f;
 
 		for (x = 0, s = r; x < mkpwa->length; x++) {
-_tryagainu:		c = (char)tf_prng_range_r(rndata, (TF_UNIT_TYPE)S, (TF_UNIT_TYPE)E);
+_tryagainu:		c = (char)tf_prng_range_r(rndata, (TF_UNIT_TYPE)mkpwa->cs, (TF_UNIT_TYPE)mkpwa->ce);
 			if (mkpwa->charset) {
 				if (strchr(mkpwa->charset, c)) {
 					*s = c;

mkpwd.h
@@ -22,7 +22,7 @@ struct mkpwd_args {
 	size_t szsalt;
 
 	short format;
-	char *charset;
+	char *charset, cs, ce;
 	size_t turns;
 	size_t offset;
 	size_t length;